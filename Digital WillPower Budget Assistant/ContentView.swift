90fa306fa9c078840b16b478838ee98e7ebda037
wpage <wpage@andrew.cmu.edu>
2024-11-06 10:01:14 -0500

    Initial Commit

---
+++ Digital WillPower Budget Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Digital WillPower Budget Assistant
+//
+//  Created by Will Page on 11/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
